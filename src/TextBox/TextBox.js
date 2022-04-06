70bec1fe5b26b5c1df3dca15793772c4b3457e5f
fedor-resh <fedor_20052005@mail.ru>
2022-04-06 23:20:50 +0300

    refactor and add effect of disable text

--- src/TextBox/TextBox.js
+++ src/TextBox/TextBox.js
@@ -5,8 +5,8 @@ import '../fonts/robotomono.css'
 
 const TextBox = () => {
     const [text, setText] = useState('lorem ipsum dolor sit amet consectetur adipisicing elit accusamus consequuntur cum cumque cupiditate deserunt distinctio illum laboriosam nesciunt nulla obcaecati optio quidem reprehenderit saepe sed sunt veritatis voluptas voluptate voluptatibus')
-    const [cursorPosition, setCursorPosition] = useState(0)
     const [currentColumn, setCurrentColumn] = useState(0)
+    const [indexOfCurrentCharacter, setIndexOfCurrentCharacter] = useState(0)
     const [currentLine, setCurrentLine] = useState(0)
     const [lengthOfLines, setLengthOfLines] = useState(0)
 
@@ -17,7 +17,7 @@ const TextBox = () => {
         const LinesLength = []
         let sum = 0
         lengthOfWords.forEach((length) => {
-            if ((sum + length) * 14.4 > textBoxWidth) {
+            if ((sum + length) * 14.9 > textBoxWidth) {
                 LinesLength.push(sum)
                 sum = 0
             }
@@ -25,7 +25,7 @@ const TextBox = () => {
         })
         console.log(LinesLength)
         setLengthOfLines(LinesLength)
-    }, [window.innerWidth])
+    }, [])
 
     useEffect(() => {
         window.addEventListener('keydown', keyboardHandler);
@@ -41,20 +41,20 @@ const TextBox = () => {
 
         const keyboardCharacter = (String.fromCharCode(e.keyCode).toLowerCase())
         // console.log('current character: '+keyboardCharacter)
-        // console.log('current in text: '+text[cursorPosition], cursorPosition)
-        let curPosition = cursorPosition
+        // console.log('current in text: '+text[currentColumn], currentColumn)
+        let curPosition = currentColumn
         let curLine = currentLine
-        if (keyboardCharacter === text[currentColumn]) {
+        if (keyboardCharacter === text[indexOfCurrentCharacter]) {
             curPosition++
             if(curPosition === lengthOfLines[currentLine]){
                 curPosition = 0
                 curLine++
             }
-            cursorRef.current.style.top = `${(curLine) * 29.5 + 23}px`
-            cursorRef.current.style.left = `${(curPosition) * 14.4}px`
-            setCursorPosition(curPosition)
+            cursorRef.current.style.top = `${(curLine) * 38 + 28}px`
+            cursorRef.current.style.left = `${(curPosition) * 14.9 - 1}px`
+            setCurrentColumn(curPosition)
             setCurrentLine(curLine)
-            setCurrentColumn(currentColumn+1)
+            setIndexOfCurrentCharacter(indexOfCurrentCharacter+1)
             // console.log(curPosition)
         }
     }
@@ -63,7 +63,9 @@ const TextBox = () => {
         <div className={s.text__wrapper}>
             <div ref={cursorRef} className={s.cursor}/>
             <p ref={textRef} className={s.text}>
-                {text}
+                {Array.from(text).map((character,id)=>
+                    <letter className={id>=indexOfCurrentCharacter?s.disabled__letter:''} key={id}>{character}</letter>
+                )}
             </p>
         </div>
     );
