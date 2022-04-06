251b57770ece22b4b709bb23abd1905cac862c5b
fedor-resh <fedor_20052005@mail.ru>
2022-04-06 21:37:11 +0300

    add math m oves of cursor

--- src/TextBox/TextBox.js
+++ src/TextBox/TextBox.js
@@ -1,33 +1,68 @@
 import React, {useEffect, useRef, useState} from 'react';
 import s from './TextBox.module.css'
 import '../fonts/robotomono.css'
-import {keyboard} from '@testing-library/user-event/dist/keyboard';
+
 
 const TextBox = () => {
-    const [text, setText] = useState('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, consequuntur cum cumque cupiditate deserunt distinctio, illum, laboriosam nesciunt nulla obcaecati optio quidem reprehenderit saepe sed sunt veritatis voluptas voluptate voluptatibus?')
+    const [text, setText] = useState('lorem ipsum dolor sit amet consectetur adipisicing elit accusamus consequuntur cum cumque cupiditate deserunt distinctio illum laboriosam nesciunt nulla obcaecati optio quidem reprehenderit saepe sed sunt veritatis voluptas voluptate voluptatibus')
     const [cursorPosition, setCursorPosition] = useState(0)
-    const [currentCharacter, setCurrentCharacter] = useState(' ')
-    useEffect(()=>{
+    const [currentColumn, setCurrentColumn] = useState(0)
+    const [currentLine, setCurrentLine] = useState(0)
+    const [lengthOfLines, setLengthOfLines] = useState(0)
+
+    useEffect(() => {
+        const textBoxWidth = textRef.current.scrollWidth
+        const words = textRef.current.textContent.split(/\s/)
+        const lengthOfWords = words.map((word) => word.length)
+        const LinesLength = []
+        let sum = 0
+        lengthOfWords.forEach((length) => {
+            if ((sum + length) * 14.4 > textBoxWidth) {
+                LinesLength.push(sum)
+                sum = 0
+            }
+            sum += length + 1
+        })
+        console.log(LinesLength)
+        setLengthOfLines(LinesLength)
+    }, [window.innerWidth])
+
+    useEffect(() => {
         window.addEventListener('keydown', keyboardHandler);
-    },[])
-    const cursor = useRef()
+        return () => {
+            window.removeEventListener('keydown', keyboardHandler)
+        }
+    }, [keyboardHandler])
+    const cursorRef = useRef()
+    const textRef = useRef()
+
 
     function keyboardHandler(e) {
-        console.log(String.fromCharCode(e.keyCode).charCodeAt(0))
-        console.log('...'+text[cursorPosition].charCodeAt(0))
-        setCurrentCharacter(String.fromCharCode(e.keyCode))
-        if(String.fromCharCode(e.keyCode) === text[cursorPosition]){
-            cursor.current.style.left = `${(cursorPosition+1)*10}px`
-            setCursorPosition(cursorPosition+1)
-            console.log( `${cursorPosition*10}px`)
 
+        const keyboardCharacter = (String.fromCharCode(e.keyCode).toLowerCase())
+        // console.log('current character: '+keyboardCharacter)
+        // console.log('current in text: '+text[cursorPosition], cursorPosition)
+        let curPosition = cursorPosition
+        let curLine = currentLine
+        if (keyboardCharacter === text[currentColumn]) {
+            curPosition++
+            if(curPosition === lengthOfLines[currentLine]){
+                curPosition = 0
+                curLine++
+            }
+            cursorRef.current.style.top = `${(curLine) * 29.5 + 23}px`
+            cursorRef.current.style.left = `${(curPosition) * 14.4}px`
+            setCursorPosition(curPosition)
+            setCurrentLine(curLine)
+            setCurrentColumn(currentColumn+1)
+            // console.log(curPosition)
         }
     }
 
     return (
         <div className={s.text__wrapper}>
-            <div ref={cursor} className={s.cursor}/>
-            <p className={s.text}>
+            <div ref={cursorRef} className={s.cursor}/>
+            <p ref={textRef} className={s.text}>
                 {text}
             </p>
         </div>
